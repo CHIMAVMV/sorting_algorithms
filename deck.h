7eb9d5d8f4e962ab7cbb469071c0e523e7c83749
Nweke Chima Gideon <nweke_chima@yahoo.com>
2023-02-24 14:39:26 -0800

    sort

--- deck.h
+++ deck.h
@@ -1,10 +1,15 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <stdlib.h>
+
 typedef enum kind_e
 {
     SPADE = 0,
     HEART,
     CLUB,
     DIAMOND
-} kind_e;
+} kind_t;
 
 /**
  * struct card_s - Playing card
@@ -17,7 +22,7 @@ typedef struct card_s
 {
     const char *value;
     const kind_t kind;
-} card_s;
+} card_t;
 
 /**
  * struct deck_node_s - Deck of card
@@ -31,4 +36,7 @@ typedef struct deck_node_s
     const card_t *card;
     struct deck_node_s *prev;
     struct deck_node_s *next;
-} deck_node_s;
+} deck_node_t;
+void sort_deck(deck_node_t **deck);
+
+#endif /* DECK_H */
